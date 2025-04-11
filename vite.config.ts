6b11d8180c5982a16d227e18a1793605d56b48ed
Rishabh Pathak <123829611+RishabhPathak93@users.noreply.github.com>
2025-04-11 11:01:59 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-	plugins: [react()],
+  plugins: [react()],
+  publicDir: 'public', // this is default, just ensure it's not missing or renamed
 });
